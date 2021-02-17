62773a7abc7648d3beb9a69fc83bf326e3cf6c26
LAPTOP-4S09PJE1\petra <pege@appvestor.com>
2021-02-17 20:19:19 +0100

    test user

---
+++ assets/js/stickymenu.js
@@ -0,0 +1,29 @@
+// const header = document.querySelector("header");
+
+// $(document).ready(function () {
+    
+
+//   if ($(window).width() >= 905) {
+//     //sticky menu & fade animation on hover
+    
+//     $(window).scroll(function () {
+        
+//       if ($(this).scrollTop() > 76) {
+//         $(header).addClass("sticky");
+//         $(header).css("opacity", ".2");
+//         $(header).hover(
+//           function () {
+//             $(header).css("opacity", "1");
+//           },
+//           function () {
+//             $(header).css("opacity", ".2");
+//           }
+//         );
+//       } else {
+//         $(header).removeClass("sticky");
+//         $(header).css("opacity", "1");
+//         $(header).off('mouseenter mouseleave'); 
+//       }
+//     });
+//   }
+// });
